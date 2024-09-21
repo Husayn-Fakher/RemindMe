9f84511117cd6d176b2e01a04296ce138b128b92
Husayn-Fakher <husayn.fakher@gmail.com>
2024-09-21 12:26:59 +0200

    Second Commit

--- app/src/main/java/com/example/september24/MainActivity.kt
+++ app/src/main/java/com/example/september24/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello, How are you $name?",
         modifier = modifier
     )
 }
